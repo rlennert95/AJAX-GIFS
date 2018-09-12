edd6ef9af43bae67864cd23fbafc0476ef442712
GodZkotm54 <41457922+rlennert95@users.noreply.github.com>
2018-09-12 18:10:04 -0400

    add assets/

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,25 @@
 var animals = ["Cats", "Dogs", "Birds", "Fish",];
 
+function buttonWork() { 
+    var animal =  $("#searchbox").val();
+    console.log(animal);
+        var queryURL = "https://api.giphy.com/v1/gifs/random?q=&api_key=XTfIqK52UP014oNtjgckUK4TpsGCFzyT&tag=" + animal;
+
+     $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+            .then(function (response) {
+                console.log(response);
+                var imageUrl = response.data.images.fixed_height.url;
+                console.log(imageUrl);
+                var selectedImage = $("<img src='" + imageUrl + "'>");
+                console.log(selectedImage);
+
+                $("#images").prepend(selectedImage);
+            });
+}
+
 function renderButtons () {
     $("#buttons").empty();
     for (var i = 0; i < animals.length; i++) {
@@ -11,39 +31,24 @@ function renderButtons () {
     }
 }
 
+
 $(document).ready(function () {
+
+
+    renderButtons();
 
     $("#submit").on("click", function (event) {
         event.preventDefault();
         var searchTerm = $("#searchbox").val();
-        $("#searchbox").text("");
-        $("#buttons").append("<button id='button'>" + searchTerm + "</button>");
-
         animals.push(searchTerm);
+        renderButtons(); 
         console.log(animals);
 
     });
 
     $("#buttons").on("click", function (event) {
         event.preventDefault();
-        var searchTerm = $("#searchbox").val();
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=&api_key=XTfIqK52UP014oNtjgckUK4TpsGCFzyT&q=" + searchTerm;
-        console.log(searchTerm);
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                console.log(response);
-                var imageUrl = response.data[0].images.fixed_height.url;
-                console.log(imageUrl);
-                var selectedImage = $("<img src='" + imageUrl + "'>");
-                console.log(selectedImage);
-
-                $("#images").prepend(selectedImage);
-            });
-
-
+        buttonWork();
     });
 
 });
